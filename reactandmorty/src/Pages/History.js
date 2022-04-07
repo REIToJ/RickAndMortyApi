e48dc60007ed25657f095d13ea6f59b0cecf07b8
REIToJ <57962348+REIToJ@users.noreply.github.com>
2022-04-07 15:35:36 +0300

    Add files via upload

---
+++ reactandmorty/src/Pages/History.js
@@ -0,0 +1,15 @@
+import React, {useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+
+const History = () => {
+  
+  
+  
+  return(
+    <div className="">
+      There was 
+    </div>
+  )
+}
+
+export default History
